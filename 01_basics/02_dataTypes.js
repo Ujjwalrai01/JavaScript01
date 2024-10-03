12e2abd06894b23f6068c4cb581c2fab5a97b769
Ujjwalrai01 <152022994+Ujjwalrai01@users.noreply.github.com>
2024-10-03 18:49:20 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+ "use strict"; // treat all JS code as newer version
+ 
+//  alert( 3 + 3 ) // we are using nodejs, not browser
+console.log(3+3);
+
+console.log(Ujjwal_Rai);
+
+let name = "Ujjwal" // string dataType
+let age = 18  // numbers 
+let isLoggedIn = false // boolean tye dataType
+
+// types of dataTypes//
+
+// number => 2 to power 53
+//bigint
+//string => "" or '' / prefer to use double quote("")
+// boolean => true/false
+// null => standalone value
+// undefined
+//symbol => unique
+
+//object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
